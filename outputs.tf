ce9686722f1e3a12f3c1335efb2bd878d9a78228
James Armes <jarmes@codeforamerica.org>
2025-06-23 13:37:39 -0400

    ci: Updated spacing.

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "kms_key_alias" {
   description = "Alias for of the KMS key used for encryption."
-  value = aws_kms_alias.secrets.name
+  value       = aws_kms_alias.secrets.name
 }
 
 output "kms_key_arn" {
   description = "ARN for of the KMS key used for encryption."
-  value = aws_kms_key.secrets.arn
+  value       = aws_kms_key.secrets.arn
 }
 
 output "secrets" {
   description = "A map of created secrets."
-  value = module.secrets_manager
+  value       = module.secrets_manager
 }
